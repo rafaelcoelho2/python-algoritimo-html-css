50acaded6169728c0cd8784f3b1d6c8fbf951203
rafaelcoelho2 <150600441+rafaelcoelho2@users.noreply.github.com>
2024-05-25 18:28:07 -0300

    Update media-notas.py

--- media-notas.py
+++ media-notas.py
@@ -1,1 +1,9 @@
 
+notas = []
+for i in range(3):
+     aluno = str(input('digite o nome do aluno: '))
+     nota = float(input('notas : '))
+     notas.append(nota)
+media = sum(notas)/len(notas) 
+print('minhas notas: ', notas)
+print('sua média é: ', media)
